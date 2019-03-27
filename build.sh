90fc939ef0dd671d56abdfc67706034e66a4cb08
Hayakuchi0 <hayakuchi@hinesm.info>
2019-03-27 10:49:27 +0900

    fix build tool

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+origin=`pwd`
 here=`dirname ${0}`
 here=`cd ${here};pwd`
+cd ${here}
 function build_takeyari()
 {
   tsc && node store_bundle/store_src/main.js && ng build --prod
@@ -9,16 +11,20 @@ function exist_check()
 {
   if !(type ${1} > /dev/null 2>&1)
   then
-    if (type "apt" > /dev/null 2>&1)
-    then
-      echo "install ${2} now."
-      sudo apt install ${2}
-      echo "installed ${2}"
-    fi
     if (type "yum" > /dev/null 2>&1)
     then
       echo "install ${3} now."
       sudo yum install ${3}
+      echo "installed ${3}"
+    elif (type "apt" > /dev/null 2>&1)
+    then
+      echo "install ${2} now."
+      sudo apt install ${2}
+      echo "installed ${2}"
+    elif (type "pkg" > /dev/null 2>&1)
+    then
+      echo "install ${3} now."
+      sudo pkg install ${3}
       echo "installed ${3}"
     fi
   fi
@@ -28,3 +34,5 @@ exist_check npm npm npm
 npm install
 PATH="${PATH}:${here}/node_modules/.bin"
 build_takeyari
+cd ${origin}
+echo "complete building"

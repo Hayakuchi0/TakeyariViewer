4b95cebde863fa2eee99549fa29440bd65aa57fe
Hayakuchi0 <hayakuchi@hinesm.info>
2019-05-19 16:40:47 +0900

    Shift-JISを使うとftpが使えない不具合を修正

--- store_src/send/sender.ts
+++ store_src/send/sender.ts
@@ -1,11 +1,13 @@
 const fs = require('fs-extra');
 const path = require('path');
+const iconv = require('iconv-lite');
 export abstract class Sender {
   encoding:string;
   config:JSON;
   constructor() {
     this.encoding = fs.readFileSync(path.join(process.cwd(),'CONFIG','encoding.txt'),"utf-8").split("\n")[0];
-    this.config = JSON.parse(fs.readFileSync(path.join(process.cwd(),'CONFIG','send',this.protocolName()+'.json'), this.encoding));
+    let buffer = fs.readFileSync(path.join(process.cwd(),'CONFIG','send',this.protocolName()+'.json'));
+    this.config = JSON.parse(iconv.decode(buffer, this.encoding));
     this.readconfig();
   }
   abstract protocolName():string;
